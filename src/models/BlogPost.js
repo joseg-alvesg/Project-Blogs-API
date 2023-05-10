860967a1216fa9ddb90a764e94ead1eff6c449ea
joseg-alvesg <cinhoalves85@gmail.com>
2023-05-10 17:54:48 -0300

    create: `models`

---
+++ src/models/BlogPost.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Blog_Posts = sequelize.define('blog_posts', {
+    id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    underscored: true,
+  })  
+
+  return Blog_Posts;
+};
---
+++ src/models/Categories.js
@@ -0,0 +1,8 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  const Categories = sequelize.define('Categories', {
+    name: DataTypes.STRING,
+  }, { timestamps: false })
+  return Categories;
+};
