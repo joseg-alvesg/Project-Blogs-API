7f2ea08e5b4b881d911333898b16f7483ab38d93
joseg-alvesg <cinhoalves85@gmail.com>
2023-05-12 19:16:40 -0300

    fix: :ambulance:

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,7 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const category = sequelize.define('Category', {
-    id: {type: DataTypes.INTEGER, primaryKey: true},
     name: DataTypes.STRING,
   }, { timestamps: false })
   
