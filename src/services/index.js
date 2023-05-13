d078906165fb31ecf047d965ea231e754ddcfdb6
joseg-alvesg <cinhoalves85@gmail.com>
2023-05-13 17:48:48 -0300

    implements: `BlogPost` service
    need refact

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const userService = require('./user.service');
 const categoryService = require('./categoies.service');
+const blogPost = require('./post.service');
 
 module.exports = {
   userService,
   categoryService,
+  blogPost,
 };
---
+++ src/services/post.service.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+const { BlogPost, Category, PostCategory } = require('../models');
+
+const insertCategory = async (postId, categoryId) =>
+  PostCategory.create({ postId, categoryId });
+
+const insert = async ({ title, content, categoryIds }, user) => {
+  const categories = await Promise.all(
+    categoryIds.map((id) => Category.findByPk(id)),
+  ).then((c) => c.every((id) => id));
+  if (!categories) return undefined;
+
+  const decoded = jwt.decode(user, { complete: true });
+  const userId = await User.findOne({
+    where: { email: decoded.payload.email },
+  }).then((a) => a.dataValues.id);
+
+  const data = await BlogPost.create({ title, content, userId });
+  const postCategories = await Promise.all(
+    categoryIds.map((categoryId) => insertCategory(data.dataValues.id, categoryId)),
+  );
+  return data;
+};
+
+module.exports = {
+  insert,
+};
